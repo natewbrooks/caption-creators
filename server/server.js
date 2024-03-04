22fabcfaf50e8b5800660ec814ee61e6fba6bdfb
nate <59595919+natewbrooks@users.noreply.github.com>
2024-03-04 14:50:06 -0500

    Handles logic for lobby session restoration on reload
    Gives 10 second grace period to a user that disconnects so that they can seamlessly rejoin and reestablish their connection. Tracks userToken on reload because socketId always changes.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const { createServer } = require('http');
 const { Server } = require('socket.io');
 const next = require('next');
 const { customAlphabet } = require('nanoid');
+const { join } = require('path');
 const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'; // No hyphens or underscores (default has them)
 const nanoid = customAlphabet(alphabet, 7);
 
@@ -11,6 +12,7 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const lobbies = {}; // Maps lobby IDs to lobby details
+const disconnectQueue = []; // Add users to this list before disconnecting them so they have time to restore their session.
 
 app.prepare().then(() => {
 	const server = createServer((req, res) => handle(req, res));
@@ -23,7 +25,7 @@ app.prepare().then(() => {
 	});
 
 	io.on('connection', (socket) => {
-		console.log('User connected');
+		console.log('New user connected to server');
 
 		socket.on('create_lobby', ({ hostUserToken }) => createLobby(socket, hostUserToken, io));
 		socket.on('join_lobby', (data) => joinLobby(socket, data, io));
@@ -32,7 +34,56 @@ app.prepare().then(() => {
 		);
 		socket.on('fetch_lobby_details', (data) => fetchLobbyDetails(socket, data, io));
 		socket.on('leave_lobby', () => leaveLobby(socket, io));
-		socket.on('disconnect', () => leaveLobby(socket, io));
+
+		socket.on('reload', (userToken) => {
+			console.log(`Reload event received for userToken: ${userToken}`);
+			disconnectQueue.push(userToken);
+			console.log(`Current disconnectQueue: ${JSON.stringify(disconnectQueue)}`);
+		});
+
+		socket.on('disconnect', () => {
+			// Get the disconnecting user's userToken before the socketId changes
+			const disconnectingUser = findUserToken(socket.id);
+			// If there was a page reload, the disconnecting user would have been added to the disconnect queue.
+			if (disconnectingUser && disconnectQueue.includes(disconnectingUser)) {
+				console.log('Start timeout for session');
+				setTimeout(() => {
+					// If the user has reconnected before the timeout finishes, then it won't trigger a disconnect
+					if (!disconnectQueue.includes(disconnectingUser)) {
+						console.log(disconnectingUser + ' user session restored!');
+					} else {
+						leaveLobby(socket, io);
+					}
+				}, 10000); // Adjust the timeout duration as needed
+			} else {
+				// If the player closes the tab, then we want to immediately disconnect
+				leaveLobby(socket, io);
+			}
+		});
+
+		// If the connected player is in the disconnect queue, remove them from the queue because they have restored their session.
+		socket.on('restore_session', (userToken) => {
+			if (disconnectQueue.includes(userToken)) {
+				disconnectQueue.splice(disconnectQueue.indexOf(userToken), 1);
+
+				const lobbyId = findLobbyId(userToken);
+				if (lobbyId) {
+					// Update the user's socket ID in the lobby to handle the new connection
+					updateUserSocketId(lobbyId, userToken, socket.id, io);
+
+					// This ensures the user's socket is subscribed to the lobby's room again
+					socket.join(lobbyId);
+					console.log(
+						`User ${userToken} reconnected and joined lobby ${lobbyId} with new socket ID ${socket.id}`
+					);
+
+					// Send an update to all members of the lobby about the reconnection
+					updateLobby(lobbyId, io);
+				} else {
+					console.log(`Lobby not found for userToken ${userToken}. Cannot restore session.`);
+				}
+			}
+		});
 	});
 
 	server.listen(port, () => console.log(`> Ready on http://localhost:${port}`));
@@ -44,6 +95,30 @@ function findPlayerInLobby(lobbyId, userToken) {
 	if (!lobby) return null; // Return null if the lobby doesn't exist.
 	const player = lobby.members.find((member) => member.userToken === userToken);
 	return player || null; // Return the found player or null if not found.
+}
+
+// Searches for a corresponding userToken given a socketId
+function findUserToken(socketId) {
+	for (const lobbyId in lobbies) {
+		const lobby = lobbies[lobbyId];
+		const member = lobby.members.find((member) => member.id === socketId);
+		if (member) {
+			return member.userToken;
+		}
+	}
+	return null; // Return null if the user token is not found for the provided socket ID.
+}
+
+// Searches for a lobbyId of a given userToken
+function findLobbyId(userToken) {
+	for (const lobbyId in lobbies) {
+		const lobby = lobbies[lobbyId];
+		const member = lobby.members.find((member) => member.userToken === userToken);
+		if (member) {
+			return lobbyId;
+		}
+	}
+	return null; // Return null if the user token is not found in any lobby.
 }
 
 // Creates a new lobby with a unique ID and adds the host as the first member.
@@ -59,6 +134,7 @@ function createLobby(socket, hostUserToken, io) {
 	socket.join(lobbyId); // Add the host's socket to the lobby room.
 	updateLobby(lobbyId, io); // Update all clients with the new lobby details.
 	socket.emit('lobby_created', { lobbyId, hostUserToken }); // Notify the host that the lobby has been created.
+	console.log('Created new lobby: ' + lobbyId);
 }
 
 // Allows a player to join an existing lobby if it exists and they are not already a member.
@@ -85,6 +161,52 @@ function joinLobby(socket, { lobbyId, playerName, userToken }, io) {
 	socket.join(lobbyId); // Add the player's socket to the lobby room.
 	updateLobby(lobbyId, io); // Update all clients with the new lobby details.
 	socket.emit('lobby_joined', { lobbyId }); // Notify the player that they have joined the lobby.
+	console.log('User connected to lobby: ' + lobbyId);
+}
+
+// Sends updated lobby details to all clients in the lobby.
+function updateLobby(lobbyId, io) {
+	const lobby = lobbies[lobbyId];
+	if (!lobby) return; // Do nothing if the lobby doesn't exist.
+	io.to(lobbyId).emit('update_lobby', {
+		members: lobby.members,
+		hostUserToken: lobby.hostUserToken,
+	});
+}
+
+// Handles a player leaving a lobby, including deleting the lobby if it becomes empty, and reassigning the host.
+function leaveLobby(socket, io) {
+	Object.keys(lobbies).forEach((lobbyId) => {
+		const lobby = lobbies[lobbyId];
+		const index = lobby.members.findIndex((member) => member.id === socket.id);
+		if (index !== -1) {
+			const disconnectedMember = lobby.members.splice(index, 1)[0]; // Remove the player from the lobby.
+			console.log(
+				(disconnectedMember.isHost ? 'Host' : 'User') + ' disconnected from lobby: ' + lobbyId
+			);
+
+			if (lobby.members.length === 0) {
+				delete lobbies[lobbyId]; // Delete the lobby if it's empty.
+			} else {
+				// Check if the disconnected player was the host
+				if (disconnectedMember.isHost) {
+					// Select a random member to be the new host
+					const newHostIndex = Math.floor(Math.random() * lobby.members.length);
+					lobby.members[newHostIndex].isHost = true;
+					lobby.hostUserToken = lobby.members[newHostIndex].userToken;
+					console.log('Lobby ' + lobbyId + ' has a new host: ' + lobby.hostUserToken);
+				}
+			}
+
+			updateLobby(lobbyId, io); // Update all clients with the new lobby details.
+
+			// If there are no members left in the lobby, delete the lobby
+			if (lobby.members.length === 0) {
+				console.log(`Lobby ${lobbyId} is now empty and will be deleted.`);
+				delete lobbies[lobbyId];
+			}
+		}
+	});
 }
 
 // Fetches and sends the details of a specific lobby to a requesting client.
@@ -109,42 +231,19 @@ function updatePlayerName(lobbyId, userToken, playerName, io) {
 	}
 }
 
-// Sends updated lobby details to all clients in the lobby.
-function updateLobby(lobbyId, io) {
+// Function to update a user's socket ID in a lobby
+function updateUserSocketId(lobbyId, userToken, newSocketId, io) {
 	const lobby = lobbies[lobbyId];
-	if (!lobby) return; // Do nothing if the lobby doesn't exist.
-	io.to(lobbyId).emit('update_lobby', {
-		members: lobby.members,
-		hostUserToken: lobby.hostUserToken,
-	});
-}
+	if (!lobby) {
+		console.log(`Lobby ${lobbyId} not found.`);
+		return;
+	}
 
-// Handles a player leaving a lobby, including deleting the lobby if it becomes empty, and reassigning the host.
-function leaveLobby(socket, io) {
-	Object.keys(lobbies).forEach((lobbyId) => {
-		const lobby = lobbies[lobbyId];
-		const index = lobby.members.findIndex((member) => member.id === socket.id);
-		if (index !== -1) {
-			const disconnectedMember = lobby.members.splice(index, 1)[0]; // Remove the player from the lobby.
-			if (lobby.members.length === 0) {
-				delete lobbies[lobbyId]; // Delete the lobby if it's empty.
-			} else {
-				// Check if the disconnected player was the host
-				if (disconnectedMember.isHost) {
-					// Select a random member to be the new host
-					const newHostIndex = Math.floor(Math.random() * lobby.members.length);
-					lobby.members[newHostIndex].isHost = true;
-					lobby.hostUserToken = lobby.members[newHostIndex].userToken;
-				}
-			}
-
-			updateLobby(lobbyId, io); // Update all clients with the new lobby details.
-
-			// If there are no members left in the lobby, delete the lobby
-			if (lobby.members.length === 0) {
-				console.log(`Lobby ${lobbyId} is now empty and will be deleted.`);
-				delete lobbies[lobbyId];
-			}
-		}
-	});
+	const member = lobby.members.find((member) => member.userToken === userToken);
+	if (member) {
+		member.id = newSocketId; // Update the member's socket ID
+		updateLobby(lobbyId, io); // Reflect this change to all members of the lobby
+	} else {
+		console.log(`User with userToken ${userToken} not found in lobby ${lobbyId}.`);
+	}
 }
